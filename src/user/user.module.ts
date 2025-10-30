ad801e0c13195a215b5e64ec9d1c9900c9825883
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2025-10-31 01:41:38 +0400

    fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule as Mongoose } from '@nestjs/mongoose';
-import { AuthControllerV1 } from './controllers/auth.controller.v1';
-import { AuthService } from './services/auth.service';
-import { UserService } from './services/user.service';
-import { UserControllerV1 } from './controllers/user.controller.v1';
-import { JwtModuleCustom } from '@shared/modules/jwt/jwt.module';
 import { UserSchema } from './schemas/UserShema';
 
 @Module({
 	imports: [
         Mongoose.forFeature([{ name: 'User', schema: UserSchema }]),
-        JwtModuleCustom,
 		
 	],
-	controllers: [AuthControllerV1, UserControllerV1],
-	providers: [
-		AuthService,
-		UserService,
-	],
-	exports: [AuthService, UserService ],
+	controllers: [],
+	providers: [],
+	exports: [],
 })
 export class UserModule {}
